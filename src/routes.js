92323ac70a23236fcec498fda391f8b04cfc11d0
Gabriel Batista <svbgabriel@gmail.com>
2019-12-11 22:01:24 -0300

    Converte a rota para um componente funcional

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './pages/Main';
 import Box from './pages/Box';
 
-
-const Routes = () => (
+export default function Routes() {
+  return (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={Main} />
-            <Route path="/box/:id" component={Box} />
-        </Switch>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/box/:id" component={Box} />
+      </Switch>
     </BrowserRouter>
-);
-
-export default Routes;
+  );
+}
