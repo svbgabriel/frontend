97dab756e020b426c9b65882cbedc6e8cb5afcc8
Gabriel Batista <svbgabriel@gmail.com>
2019-12-11 22:02:19 -0300

    Converte a página Main para um componente funcional

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
-export default class Main extends Component {
-    state = {
-        newBox: ''
-    };
-    
-    handleSubmit = async e => {
-        e.preventDefault();
-        
-        const response = await api.post('boxes', {
-            title: this.state.newBox
-        });
+export default function Main({ history }) {
+  const [newBox, setNewBox] = useState('');
 
-        this.props.history.push(`/box/${response.data._id}`);
-    }
+  const handleSubmit = async e => {
+    e.preventDefault();
 
-    handleInputChange = e => {
-        this.setState({newBox: e.target.value});
-    }
-  
-    render() {
-    return (
-        <div id="main-container">
-            <form onSubmit={this.handleSubmit}>
-                <img src={logo} alt="" />
-                <input
-                    placeholder="Criar um box" value={this.state.newBox} onChange={this.handleInputChange}
-                />
-                <button type="submit">Criar</button>
-            </form>
-        </div>
-    );
-  }
+    const response = await api.post('boxes', {
+      title: newBox,
+    });
+
+    history.push(`/box/${response.data._id}`);
+  };
+
+  const handleInputChange = e => setNewBox(e.target.value);
+
+  return (
+    <div id="main-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="" />
+        <input
+          placeholder="Criar um box"
+          value={newBox}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Criar</button>
+      </form>
+    </div>
+  );
 }
